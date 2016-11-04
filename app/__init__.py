794b49661ac86cce35c3a4de619d99231aed4bc1
Bing Xu <wenbin147@126.com>
2016-11-04 23:20:47 +0800

    moment fail

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 # -*- coding: UTF-8 -*-
 from flask import Flask#程序包的构造文件
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 # from config import config
 
 bootstrap=Bootstrap()#实例化
+moment=Moment()
 
 def creat_new():
     appnew=Flask(__name__)
@@ -12,6 +14,7 @@ def creat_new():
     # config[config_name].init_app(appnew)
 
     bootstrap.init_app(appnew)#初始化
+    moment.init_app(appnew)
 
 
     from .mainnew_blueprint import mainnew
--- app/mainnew_blueprint/views.py
+++ app/mainnew_blueprint/views.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 from flask import render_template
+from datetime import datetime
 from . import mainnew#导入mainnew蓝本
 
 @mainnew.route('/<name>')#mainnew蓝本内路由
